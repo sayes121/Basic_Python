8226d430ee8ce8b5e5de2a5585c24bd04b83589e
Sayee <sayes121@outlook.com>
2023-02-24 02:33:03 +1000

    Chapter 1 - Python Introduction
    Chapter 1

---
+++ Python for Beginners/Python_Intro.py
@@ -0,0 +1,26 @@
+print('Hello World')
+
+#Swap between two numbers
+a,b = 16, 10
+print(a,b)
+a,b = b,a
+print(a,b)
+
+#How many time python is repeated in a sentence
+str = 'welcome to Python Tutorials. This is about Python. Python is an high level programming language used by python programmers.'
+print(str.count('Python'))
+
+# COMMENTS
+# 1 --> In-line Comments: one line comment
+# 2 --> Block Comment: multiple lines of comment
+
+# IDENTIFIERS
+# RESERVED WORDS
+
+# Program to print all keywords
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+
+#VARIABLES
+
