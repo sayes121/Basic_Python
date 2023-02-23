35107cdcc4968a230f476e84bb32cdffbd5ab35b
Sayee <sayes121@outlook.com>
2023-02-23 23:02:23 +1000

    Chapter - 1
    Expression Data Type and Variables are created in Python

---
+++ Automated the Boring stuff using python/Chapter-1-Expression,Datatype,Variables.py
@@ -0,0 +1,10 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #How to start programming
+>>> print('Hello World')
+Hello World
+>>> 
+>>> #We are practising Python using IDLE interactive window and text editor window is used for running a chuck of python scripts
+>>> 
+>>> #Chapter 1 - Variable, datatype and Expressions
+>>> 
